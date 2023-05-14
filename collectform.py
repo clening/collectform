26d70e40b5ca4907feb966833b8c2d0e268df911
Cat Girl <7772353+clening@users.noreply.github.com>
2023-05-14 21:17:24 +0100

    Update collectform.py
    Whacked some more things on.

--- collectform.py
+++ collectform.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
-        name = request.form.get('name')
+        first_naem = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        age = request.form.get('age')
         address = request.form.get('address')
         gender = request.form.get('gender')
         government_id = request.form.get('government_id')
@@ -14,7 +16,7 @@ def form():
 
         # write to a text file
         with open('output.txt', 'a') as f:
-            f.write(f'Name: {name}, Address: {address}, Gender: {gender}, Government ID: {government_id}, Sexual Orientation: {sexual_orientation}\n')
+            f.write(f'Name: {first.name + last_name}, Age: {age}, Address: {address}, Gender: {gender}, Government ID: {government_id}, Sexual Orientation: {sexual_orientation}\n')
 
         return 'Form submitted successfully.'
 
